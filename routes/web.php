d8ce78855186c60305380037c1f1fe28312187d9
fadyelgawly92 <fadyraafatelgawly@gmail.com>
2018-10-01 02:40:56 +0200

    used guzzle client in routes web and accept json in headers and parsed the file with get request and decode it for usage and escaped the  with an outside variable and return it to the view and loop over it in panel

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
+use GuzzleHttp\Client;
+
 Route::get('/', function () {
 
+    $client = new Client([
+        'headers' => ['content-type' => 'application/json' , 'Accept' => 'application/json'],
+        'base_uri' => 'https://spreadsheets.google.com/feeds/list/0Ai2EnLApq68edEVRNU0xdW9QX1BqQXhHRl9sWDNfQXc/od6/public/basic?alt=json',
+        // 'timeout'  => 2.0,
+    ]);
     
-    return view('welcome');
+    $response = $client->request('GET');
+
+    $my_messages = json_decode( $response->getBody()->getContents() ) ;
+    $messages =  $my_messages->feed->entry ;
+    $t = "\$t";   
+    // dd($messages[0]->content->$t);
+    return view('messages.index' , compact('messages', 't'));
 });
 
 Auth::routes();
